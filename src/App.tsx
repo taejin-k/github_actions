e62fd0c78cf24864410e5d50bf24a95e84f8ee49
taejin-k <tj202304@gmail.com>
2023-05-17 17:55:47 +0900

    fix: 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-<<<<<<< HEAD
-  return <div className="App">4</div>;
-=======
-  return <div className="App">5</div>;
->>>>>>> a892fac (fix: 5)
+  return <div className="App">6</div>;
 };
 
 export default App;
