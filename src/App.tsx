4b8b754e3a530b9ca6b643948ac39da86faf9558
taejin-k <tj202304@gmail.com>
2023-05-17 18:26:47 +0900

    fix: 101

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div className="App">4</div>;
+  return (
+    <div className="App">
+      <span>101</span>
+      <span></span>
+      <span>101</span>
+    </div>
+  );
 };
 
 export default App;
